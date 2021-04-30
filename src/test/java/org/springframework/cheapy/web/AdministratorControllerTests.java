e47c331ff71b9b8c674ae16c40c95a52586913d6
Antonio Vidal <antonio.v.p.us@gmail.com>
2021-04-30 17:45:13 +0200

    Testing de admin.

--- src/test/java/org/springframework/cheapy/web/AdministratorControllerTests.java
+++ src/test/java/org/springframework/cheapy/web/AdministratorControllerTests.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
@@ -24,6 +24,7 @@ import org.springframework.cheapy.model.User;
 import org.springframework.cheapy.model.Municipio;
 import org.springframework.cheapy.model.NuOffer;
 import org.springframework.cheapy.model.SpeedOffer;
+import org.springframework.cheapy.model.StatusOffer;
 import org.springframework.cheapy.model.TimeOffer;
 import org.springframework.cheapy.model.Usuario;
 import org.springframework.cheapy.service.ClientService;
@@ -46,7 +47,11 @@ excludeAutoConfiguration = SecurityConfiguration.class)
 class AdministratorControllerTest {
 
 	private static final String TEST_USUARIO_USERNAME = "user1";
-  private static final String TEST_CLIENT_USERNAME = "user2";
+	private static final String TEST_CLIENT_USERNAME = "user2";
+  	private static final int TEST_FOODOFFER_ID = 1;
+  	private static final int TEST_NUOFFER_ID = 1;
+  	private static final int TEST_SPEEDOFFER_ID = 1;
+  	private static final int TEST_TIMEOFFER_ID = 1;
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -68,7 +73,11 @@ class AdministratorControllerTest {
 	
 	@MockBean
 	private TimeOfferService timeOfferService;
-
+	
+	private FoodOffer fo1;
+	private NuOffer nu1;
+	private SpeedOffer sp1;
+	private TimeOffer time1;
 
 	@BeforeEach
 	void setup() {
@@ -99,6 +108,7 @@ class AdministratorControllerTest {
 		client1.setDescription("client1");
 		client1.setFood("client1");
 		client1.setUsuar(user2);
+		
 		BDDMockito.given(this.clientService.getCurrentClient()).willReturn(client1);
 		BDDMockito.given(this.clientService.findByUsername(TEST_CLIENT_USERNAME)).willReturn(client1);
 		
@@ -107,6 +117,63 @@ class AdministratorControllerTest {
 		BDDMockito.given(this.speedOfferService.findSpeedOfferActOclByUserId(1)).willReturn(new ArrayList<SpeedOffer>());
 		BDDMockito.given(this.timeOfferService.findTimeOfferActOclByUserId(1)).willReturn(new ArrayList<TimeOffer>());
 		
+		FoodOffer fo1test = new FoodOffer();
+		fo1test.setId(TEST_FOODOFFER_ID);
+		fo1test.setStart(LocalDateTime.of(2021, 12, 23, 12, 30));
+		fo1test.setEnd(LocalDateTime.of(2022, 12, 23, 12, 30));
+		fo1test.setFood("fo1test");
+		fo1test.setDiscount(1);
+		fo1test.setPrice(10.0);
+		fo1test.setStatus(StatusOffer.hidden);
+		fo1test.setCode("");
+		fo1test.setClient(client1);
+		this.fo1 = fo1test;
+		BDDMockito.given(this.foodOfferService.findFoodOfferById(TEST_FOODOFFER_ID)).willReturn(this.fo1);
+		
+		NuOffer nu1test = new NuOffer();
+		nu1test.setId(TEST_NUOFFER_ID);
+		nu1test.setStart(LocalDateTime.of(2021, 12, 23, 12, 30));
+		nu1test.setEnd(LocalDateTime.of(2022, 12, 23, 12, 30));
+		nu1test.setGold(15);
+		nu1test.setDiscountGold(15);
+		nu1test.setSilver(10);
+		nu1test.setDiscountSilver(10);
+		nu1test.setBronze(5);
+		nu1test.setDiscountBronze(5);
+		nu1test.setClient(client1);
+		nu1test.setStatus(StatusOffer.hidden);
+		nu1test.setCode("");
+		this.nu1 = nu1test;
+		BDDMockito.given(this.nuOfferService.findNuOfferById(TEST_NUOFFER_ID)).willReturn(this.nu1);
+		
+		SpeedOffer sp1test = new SpeedOffer();
+		sp1test.setId(TEST_SPEEDOFFER_ID);
+		sp1test.setStart(LocalDateTime.of(2021, 12, 23, 12, 30));
+		sp1test.setEnd(LocalDateTime.of(2022, 12, 23, 12, 30));
+		sp1test.setGold(LocalTime.of(00,05,30 ));
+		sp1test.setDiscountGold(15);
+		sp1test.setSilver(LocalTime.of(00,10,30 ));
+		sp1test.setDiscountSilver(10);
+		sp1test.setBronze(LocalTime.of(00,15,30 ));
+		sp1test.setDiscountBronze(5);
+		sp1test.setClient(client1);
+		sp1test.setStatus(StatusOffer.hidden);
+		sp1test.setCode("");
+		this.sp1 = sp1test;
+		BDDMockito.given(this.speedOfferService.findSpeedOfferById(TEST_SPEEDOFFER_ID)).willReturn(this.sp1);
+		
+		TimeOffer time1test = new TimeOffer();
+		time1test.setId(TEST_TIMEOFFER_ID);
+		time1test.setStart(LocalDateTime.of(2021, 12, 23, 12, 30));
+		time1test.setEnd(LocalDateTime.of(2022, 12, 23, 12, 30));
+		time1test.setInit(LocalTime.of(12, 00));
+		time1test.setFinish(LocalTime.of(13, 00));
+		time1test.setDiscount(10);
+		time1test.setClient(client1);
+		time1test.setStatus(StatusOffer.hidden);
+		time1test.setCode("");
+		this.time1 = time1test;
+		BDDMockito.given(this.timeOfferService.findTimeOfferById(TEST_TIMEOFFER_ID)).willReturn(this.time1);
 	}
 	
 	@WithMockUser(value = "spring", authorities = "administrator")
@@ -202,5 +269,49 @@ class AdministratorControllerTest {
 				.andExpect(view().name("redirect:/administrators/clients/page/0"));
 	}
 	
-
+	@WithMockUser(value = "spring", authorities = "administrator")
+	@Test
+	void testProcessOffersRecordForm() throws Exception {
+		mockMvc.perform(get("/administrators/offersRecord0"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("datos"))
+		.andExpect(view().name("offers/offersRecordList"));
+	}
+	
+	@WithMockUser(value = "spring", authorities = "administrator")
+	@Test
+	void testProcessShowNuForm() throws Exception {
+		mockMvc.perform(get("/administrators/offers/nu/"+TEST_NUOFFER_ID))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("nuOffer"))
+		.andExpect(view().name("offers/nu/nuOffersShow"));
+	}
+	
+	@WithMockUser(value = "spring", authorities = "administrator")
+	@Test
+	void testProcessShowFoodForm() throws Exception {
+		mockMvc.perform(get("/administrators/offers/food/"+TEST_FOODOFFER_ID)
+		.with(csrf()))
+		.andExpect(model().attributeExists("foodOffer"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("offers/food/foodOffersShow"));
+	}
+	
+	@WithMockUser(value = "spring", authorities = "administrator")
+	@Test
+	void testProcessShowSpeedForm() throws Exception {
+		mockMvc.perform(get("/administrators/offers/speed/"+TEST_SPEEDOFFER_ID))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("speedOffer"))
+		.andExpect(view().name("offers/speed/speedOffersShow"));
+	}
+	
+	@WithMockUser(value = "spring", authorities = "administrator")
+	@Test
+	void testProcessShowTimeForm() throws Exception {
+		mockMvc.perform(get("/administrators/offers/time/"+TEST_TIMEOFFER_ID))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("timeOffer"))
+		.andExpect(view().name("offers/time/timeOffersShow"));
+	}
 }
